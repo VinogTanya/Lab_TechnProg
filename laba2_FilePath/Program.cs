e658b15863acf9fcd5beaab9d63613ccab7f410a
Vinogradova Tatyana <vinogradova.tatyna.2001@mail.ru>
2023-09-20 21:05:03 +0700

    Добавьте файлы проекта.

---
+++ laba2_FilePath/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba2_FilePath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
